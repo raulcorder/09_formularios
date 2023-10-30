00b21f2f3886390afcf489d4fc3dd3e1c0d078e2
raulCorde <raulcorde@gmail.com>
2023-10-30 14:56:01 +0100

    final practica rick and Morty

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
+
+const fetchData = async () => {
+  //   console.log("obteniendo datos...");
+  try {
+    loadingData(true);
+
+    const res = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await res.json();
+    // console.log(data);
+    pintarCard(data);
+  } catch (error) {
+    console.log("error");
+  } finally {
+    loadingData(false);
+  }
+};
+
+const pintarCard = (data) => {
+  const cards = document.getElementById("card-dinamicas"); // Las constantes pueden ir arriba, pero si solo las voy a utilizar aquí, mejor incluirlas dentro
+  const templateCard = document.getElementById("template-card").content;
+  const fragment = document.createDocumentFragment();
+  //   console.log(data);
+  data.results.forEach((item) => {
+    // console.log(item);
+    const clone = templateCard.cloneNode(true);
+    clone.querySelector("h5").textContent = item.name;
+    clone.querySelector("p").textContent = item.species;
+    clone.querySelector(".card-img-top").setAttribute("src", item.image);
+
+    //guardamos en el fragment para evitar el reflow
+    fragment.appendChild(clone);
+  });
+
+  cards.appendChild(fragment);
+};
+
+const loadingData = (estado) => {
+  const loading = document.getElementById("loading");
+  if (estado) {
+    loading.classList.remove("d-none");
+  } else {
+    loading.classList.add("d-none");
+  }
+};
