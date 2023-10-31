95e2e1b7b39e090e6e60ce49876d02e4d661b1e6
raulCorde <raulcorde@gmail.com>
2023-10-31 10:51:30 +0100

    constructor

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("funcionando");
+// Función constructora = plantilla = Class
+function Persona(nombre) {
+  this.nombre = nombre;
+  this.saludar = function () {
+    return `${this.nombre} dice hola`; // revisar interpolación
+  };
+  // this.saludarIngles = function () {
+  //   return `${this.nombre} says hi!`;
+  // };
+}
+
+Persona.prototype.saludarIngles = function () {
+  return `${this.nombre} says hi!`;
+};
+
+const juanito = new Persona("juanito");
+const pedrito = new Persona("pedrito");
+
+console.log(juanito);
+// console.log(juanito.saludar());
+// console.log(pedrito.saludar());
+console.log(pedrito.saludarIngles());
+console.log(pedrito);
